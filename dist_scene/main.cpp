71725d2aaa8ac83e20d3a062efa51ae14cb45356
Gabriele Palozzi <gpalozzi@Gabrieles-iMac.local>
2014-10-29 10:27:42 +0100

    Initial Commit

---
+++ dist_scene/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dist_scene
+//
+//  Created by Gabriele Palozzi on 29/10/14.
+//  Copyright (c) 2014 Gabriele Palozzi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
